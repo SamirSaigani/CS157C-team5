33e4f83ae09b5743ea94d44e924f416c4706374f
Nachiketh-Mamidi <nachiketh1110@gmail.com>
2024-04-11 14:09:42 -0700

    fixed startup errors and added HomePage.js which displays nothing for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Define other routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
