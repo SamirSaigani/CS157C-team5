2a8c36c9b22444be7dea4fbf566ed8136e513dad
Sabur Saigani <ssaigani@sfsu.edu>
2024-04-30 22:53:53 -0700

    add admin users create function

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import path from 'path';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Define the directory where your HTML files are located
 const htmlDirectory = 'CS157c-teams';
