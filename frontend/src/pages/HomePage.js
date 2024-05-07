9e63b2186c555cbf3eda247c85bd4835f9fd3681
Russell Semsem <109185074+russellsemsem@users.noreply.github.com>
2024-05-06 22:09:45 -0700

    added deleting account feature with frontend and backend

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -72,6 +72,30 @@ const HomePage = () => {
         navigate('/', { replace: true }); // Redirect to the landing page
     };
 
+    const handleDeleteAccount = () => {
+        fetch(`http://localhost:8080/api/account/${userId}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error('Something went wrong');
+        })
+        .then(data => {
+          console.log('Account deleted successfully:', data);
+          // Add code to handle successful account deletion (e.g., redirect to login page)
+          navigate('/login', { replace: true });
+        })
+        .catch(error => {
+          console.error('Error deleting account:', error);
+          alert('Failed to delete account');
+        });
+    };
+
     /* Modal Handling for adding products */
     const handleAddProduct = () => {
         setAddProductModal(true);
@@ -124,8 +148,6 @@ const HomePage = () => {
     };
     
     const handleDelete = () => {
-        //const userId = "DN@gmail.com"; // This should come from user session or state
-      
         fetch(`http://localhost:8080/api/products/${currentProduct.id}?userId=${userId}`, {
           method: 'DELETE',
           headers: {
@@ -297,8 +319,11 @@ const HomePage = () => {
                     <h5 className="mt-3">Temp Name</h5>
                     <p>{userId}</p>
                 </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="danger" onClick={handleLogout}>
+                <Modal.Footer className="d-flex justify-content-between">
+                    <Button variant="danger" onClick={handleDeleteAccount}>
+                        Delete Account
+                    </Button>
+                    <Button variant="dark" onClick={handleLogout}>
                         Logout
                     </Button>
                 </Modal.Footer>
